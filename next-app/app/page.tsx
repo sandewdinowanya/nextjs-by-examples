7673a4e7c1c5f959a6193b1c3e65139af936014f
GSNFonseka <nowanyafonseka@gmail.com>
2026-02-04 23:46:45 +0530

    Make ProductCard component as Client Component

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,11 +1,13 @@
 import Link from "next/dist/client/link";
 import Image from "next/image";
+import ProductCard from "./components/ProductCard";
 
 export default function Home() {
   return (
     <main>
       <h1>Hello, world!</h1>
       <Link href="/users">Users</Link>
+      <ProductCard />
     </main>
   );
 }
