1aed5af8fd9e3ef95b5aa6bb78cd9ab6cf6f36de
GSNFonseka <nowanyafonseka@gmail.com>
2026-02-04 22:34:01 +0530

    Add Users and New page components

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main><h1>Hello, world!</h1></main>
+    <main>
+      <h1>Hello, world!</h1>
+      <a href="/users">Users</a>
+    </main>
   );
 }
