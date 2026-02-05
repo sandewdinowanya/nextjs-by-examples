4228f972895ae00ad5212e8dd03a14d6821e5f76
GSNFonseka <nowanyafonseka@gmail.com>
2026-02-05 23:35:51 +0530

    Add Products component and integrate with the Home page

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/dist/client/link";
 import Image from "next/image";
 import ProductCard from "./components/ProductCard";
+import Products from "./components/Products";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <h1>Hello, world!</h1>
       <Link href="/users">Users</Link>
       <ProductCard />
+      <Products />
     </main>
   );
 }
